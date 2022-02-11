27b7bf2eccaa4b6e853a3752c9354905555b2bc2
vasilii-garanin <vasilii_garanin@laptevfamily.com>
2022-02-10 22:24:04 -0600

    WIP

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const res = require('express/lib/response');
 
 const server = express();
+
+const PORT = process.env.PORT || 9000;
 
 server.use(express.json());
 server.use(cors());
 
-server.get('/api/hello', (req, res) => {
-    res.json({message: 'api is working'})
-})
+server.get('/api/hello', (req, res) =>
+{
+    res.json({ message: 'api is working' });
+});
 
-server.use('*', (req, res) => {
-    res.status(`<h1>Hello World !<h1>`)
-})
+server.use('*', (req, res) =>
+{
+    res.status(`<h1>Hello World !<h1>`);
+});
 
-server.use((err, req, res, next) => { //eslint-disable-line
+server.use((err, req, res, next) =>
+{ //eslint-disable-line
     res.status(500).json({
         message: err.message,
         stack: err.stack
-    })
-})
+    });
+});
 
-console.log(process.env.PORT, process.env.NODE_ENV);
+server.listen(PORT, () => {
+    console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`)
+});
